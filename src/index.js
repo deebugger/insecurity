70873ab568f1660cfa976d59557e011b0a336510
Dror Ben-Gai <dbengai@gmail.com>
2021-08-29 10:21:01 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+async function bootstrap() {
+  throw new Error('ouch!')
+}
+
+// don't worry, it's not real..
+const token = 'ghp_qwertyuioplkjhgfdszxcvbnmmnbvcxzasdf'
+
+bootstrap().then(initVals => {
+  console.log('>> Insecurity in progress - what could go wrong?', initVals)
+}).catch(err => {
+  console.error('Oh no! something bad happened..')
+})
+
